8df7b3ead3b5aa6c7c0c0716aad5892964ca8110
ChrisMStat <98663137+ChrisMStat@users.noreply.github.com>
2023-07-13 11:00:17 -0500

    added a new function: myfun()

---
+++ R/myfun.R
@@ -0,0 +1,12 @@
+#' @title My first function
+#'
+#' @param x A quantitative vector
+#'
+#' @return A quantitative vector, the squared values
+#' @export
+#'
+#' @examples
+#' myfun(x=1:10)
+myfun <- function(x) {
+  x^2
+}
